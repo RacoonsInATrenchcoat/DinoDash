bc76fa89f6167ac69e3466f4caeba2f4213a02a3
@RacoonsInATrenchcoat <tabitamas91@gmail.com>
2025-03-07 23:30:50 +0000

    partway done to have a mobile layout

--- Frontend/Components/src/Pages/Navbar.jsx
+++ Frontend/Components/src/Pages/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import React from "react";
 import { useScoreContext, useVolumeContext } from "../Context";
+import { useMobileContext } from "../MobileMode";
 import MusicPlayer from "../MusicPlayer";
 import "../Gamelogic.css";
 
@@ -8,39 +9,38 @@ import "../Gamelogic.css";
 const Navbar = () => {
 
   try {
+    const isMobile = useMobileContext();
     const { score } = useScoreContext();
     const { volume, setVolume } = useVolumeContext();
 
     return (
       <nav className="navbar navbar-expand-lg d-flex justify-content-between">
         {/* Left Section */}
-        <div className="navbar-left d-flex align-items-center justify-content-start">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex align-items-center">
-            <li className="nav-item">
-              <Link
-                className="nav-link"
-                aria-current="page"
-                to="/"
-                onClick={() => window.location.href = "/"} >Home</Link>
-            </li>
-            <div className="navBarDivider"></div>
-            <li className="nav-item">
-              <Link className="nav-link" to="/highscorespage">High Scores</Link>
-            </li>
-          </ul>
+        <div className={isMobile == true ? 'mobile-navbar-left' : 'navbar-left d-flex align-items-center justify-content-start'}>
+          <div className="mobile-link-container">
+            <Link
+              className={isMobile == true ? 'mobile-nav-link' : `nav-link`}
+              aria-current="page"
+              to="/"
+              onClick={() => window.location.href = "/"} >Home</Link>
+          </div>
+          <div className="navBarDivider"></div>
+          <div className="mobile-link-container">
+            <Link className={isMobile == true ? 'mobile-nav-link' : `nav-link`} to="/highscorespage">High Scores</Link>
+          </div>
         </div>
 
         {/* Center Section */}
-        <div className="navbar-center text-center flex-grow-1 justify-content-center">
+        <div className={isMobile == true ? 'mobile-navbar-center' : 'navbar-center text-center flex-grow-1 justify-content-center'}>
           <p className="sigmar-regular displayfont-text">DINO DASH</p>
         </div>
 
         {/* Right Section */}
-        <div className="navbar-right d-flex align-items-center justify-content-end">
-          <div className="score-container">
+        <div className={isMobile == true ? 'mobile-navbar-right' : 'navbar-right d-flex align-items-center justify-content-end'}>
+          <div className={isMobile == true ? "mobile-score-container" : "score-container d-flex align-items-center"}>
             <p className="game-score">Score: {score}</p>
           </div>
-          <div className="volume-control d-flex">
+          <div className={isMobile == true ? 'mobile-volume-control' : 'volume-control d-flex'}>
             <label htmlFor="volume align-items-center">Volume:</label>
             <div className="custom-slider">
               <input
@@ -53,8 +53,8 @@ const Navbar = () => {
               />
             </div>
           </div>
-          <div className="vine"></div>
         </div>
+        <div className={isMobile == true ? 'mobile-vine' : 'vine'}></div>
 
         <MusicPlayer volume={volume} />
       </nav>
